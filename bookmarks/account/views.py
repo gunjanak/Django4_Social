016db28b9a829d3f94e11e68fc4d1d4469b3b457
gunjanak <janak.klal@gmail.com>
2023-03-02 23:58:50 +0545

    Action stream done

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -10,7 +10,8 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
-#from actions.utils import create_action
+from actions.utlis import create_action
+from actions.models import Action
 
 # Create your views here.
 def user_login(request):
@@ -36,9 +37,18 @@ def user_login(request):
 @login_required
 def dashboard(request):
     print("inside dashboard")
+    #Display all actions by Default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',flat=True)
+    if following_ids:
+        #If user is following others, retrive only their actions
+        actions = actions.filter(user_id__in=following_ids)
+
+    actions = actions.select_related('user','user__profile').prefetch_related('target')[:10]
     return render(request,
                   'account/dashboard.html',
-                  {'section':'dashboard'})
+                  {'section':'dashboard',
+                   'actions':actions})
 
 def register(request):
     if request.method == 'POST':
@@ -55,6 +65,7 @@ def register(request):
 
             #create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user,'has created an account')
 
             return render(request,'account/registration_done.html',{'new_user':new_user})
         
@@ -126,7 +137,7 @@ def user_follow(request):
                     user_from=request.user,
                     user_to=user)
                 print('fuck follow')
-                #create_action(request.user, 'is following', user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
